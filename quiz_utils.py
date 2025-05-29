08316c80c292e1fab0a1fa46ea132537a79df381
Kristina Cas <m.kristinacas@gmail.com>
2025-05-30 00:27:48 +0800

    Add, Fix: Added self instance and fixed colorama import to make the init work

--- quiz_utils.py
+++ quiz_utils.py
@@ -1,8 +1,14 @@
 import os
-import colorama
-import from pyfiglet import Figlet
+from colorama import init
+from pyfiglet import Figlet
 
 class QuizUtils:
-    def clear_content():
+    def clear_content(self):
         os.system('cls' if os.name == "nt" else "clear")
+        
+    def colorama_init(self):
+        init(autoreset=True)
+    
+    def figlet_init(self):
+        self.test = Figlet(font='elite')
         
