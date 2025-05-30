c86bcfa73ad9c21abba28a3b5e6c915eaa002d79
Kristina Cas <m.kristinacas@gmail.com>
2025-05-30 14:56:36 +0800

    Add: class for the questions that will be randomized later

--- quiz_maker/ans_sample_quiz.py
+++ quiz_maker/ans_sample_quiz.py
@@ -14,3 +14,88 @@ utils = QuizUtils()
 # initialize and colorama and style of figlet
 utils.colorama_init()
 utils.figlet_init()
+
+class SampleQuestions:
+    def __init__(self, prompt, choices, correct):
+        self.prompt = prompt
+        self.choices = choices
+        self.correct = correct
+
+# list of question prompts
+question_prompts = [
+    "What is the largest ocean in the world?",
+    "What is the deepest trench in the world?",
+    "What is largest gas planet?",
+    "How much of the Earth's surface is water mass?",
+    "What is the second most abundant element in the Earth's atmosphere?",
+    "What is Earth's highest point above sea level?",
+    "How old is Earth?",
+    "What plate is the fastest-moving among all of Earth's plates?",
+    "What planet has the most moons/satelites?",
+    "What are the Earth's four major layers?"
+]
+
+# list of questions with choices and correct answer index
+sample_ques = [
+    SampleQuestions(
+        question_prompts[0],
+        ["Pacific Ocean", "Indian Ocean", "Arctic Ocean", "Atlantic Ocean"],
+        0
+    ),
+    
+    SampleQuestions(
+        question_prompts[1],
+        ["Kermadec Trench", "Tonga Trench", "Marianas Trench", "New Britain Trench"],
+        2
+    ),
+    
+    SampleQuestions(
+        question_prompts[2],
+        ["Saturn", "Neptune", "Uranus", "Jupiter"],
+        3
+    ),
+    
+    SampleQuestions(
+        question_prompts[3],
+        ["70%", "71%", "68%", "73%"],
+        1
+    ),
+    
+    SampleQuestions(
+        question_prompts[4],
+        ["Nitrogen", "Argon", "Carbon Dioxide", "Oxygen"],
+        3
+    ),
+    
+    SampleQuestions(
+        question_prompts[5],
+        ["Kangchenjunga", "Mount Ararat", "K2", "Mount Everest"],
+        3
+    ),
+    
+    SampleQuestions(
+        question_prompts[6],
+        ["About 4.54 billion years", "About 5 billion years", "About 4.6 billion years", "About 8.1 billion years"],
+        0
+    ),
+    
+    SampleQuestions(
+        question_prompts[7],
+        ["South American Plate", "North American Plate", "Pacific Plate", "African Plate"],
+        2
+    ),
+    
+    SampleQuestions(
+        question_prompts[8],
+        ["Earth", "Saturn", "Jupiter", "Uranus"],
+        1
+    ),
+    
+    SampleQuestions(
+        question_prompts[9],
+        ["Shell, Mantle, First Core, Second Core", "Crust, Mantle, Outer Core, Inner Core",
+         "Cover, Mantle, Inner Core, Outer Core", "Crust, Second Layer, Contents, Specifics"],
+        1
+    )
+]
+
