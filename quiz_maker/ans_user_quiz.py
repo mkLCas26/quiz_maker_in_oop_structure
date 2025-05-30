ee68d3b7085e7ed00066210fd9844cc7710ad77f
Kristina Cas <m.kristinacas@gmail.com>
2025-05-30 15:53:51 +0800

    Add: import of utilities and necessary libraries

---
+++ quiz_maker/ans_user_quiz.py
@@ -0,0 +1,16 @@
+# For Answering User Input Quiz
+
+# import libraries
+import os
+import random
+from colorama import Fore, Style
+import time
+
+from quiz_utils import QuizUtils
+
+# set up instance for QuizUtils
+utils = QuizUtils()
+
+# initialize and colorama and style of figlet
+utils.colorama_init()
+utils.figlet_init()
